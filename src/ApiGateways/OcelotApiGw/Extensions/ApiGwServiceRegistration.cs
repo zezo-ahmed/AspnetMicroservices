f82f95db8cd055335322977e268d7d7d52773ba1
Administrator <Administrator@WIN-29EE1M103VO>
2022-11-25 13:37:32 -0800

    add ApiGw Service Registration

---
+++ src/ApiGateways/OcelotApiGw/Extensions/ApiGwServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+
+namespace OcelotApiGw.Extensions
+{
+    public static class ApiGwServiceRegistration
+    {
+        public static IServiceCollection AddApiGwServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddOcelot() // Dependency Injection
+                    .AddCacheManager(config => config.WithDictionaryHandle()); // cache Response
+
+            builder.Host.ConfigureAppConfiguration((ctx, cfg) =>
+            {
+                Console.WriteLine($"Environment Name: {ctx.HostingEnvironment.EnvironmentName}");
+                cfg.AddJsonFile($"ocelot.{ctx.HostingEnvironment.EnvironmentName}.json", true, true);
+            })
+            .ConfigureLogging((context, logging) =>
+            {
+                logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+            });
+
+            return services;
+        }
+    }
+}
--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,25 +1,9 @@
 using Ocelot.Middleware;
-using Ocelot.Cache.CacheManager;
-using Ocelot.DependencyInjection;
+using OcelotApiGw.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddOcelot() // Dependency Injection
-    .AddCacheManager(config => config.WithDictionaryHandle()); // cache Response
-
-builder.Host
-       .ConfigureAppConfiguration((ctx, cfg) =>
-       {
-           Console.WriteLine($"Environment Name: {ctx.HostingEnvironment.EnvironmentName}");
-           cfg.AddJsonFile($"ocelot.{ctx.HostingEnvironment.EnvironmentName}.json", true, true);
-       })
-       .ConfigureLogging((context, logging) =>
-       {
-           logging.AddConfiguration(context.Configuration.GetSection("Logging"));
-           logging.AddConsole();
-           logging.AddDebug();
-       });
+builder.Services.AddApiGwServices(builder);
 
 var app = builder.Build();
 
